166b30cddb0fd81cd6295c5f75b02f9ceb761a50
Jason Miller <jason@developit.ca>
2025-06-13 22:49:22 -0400

    build opt

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
   plugins: [preact()],
   // Use the repository name for asset paths when deployed to GitHub Pages
   //base: '/pui/',
+  build: {
+    modulePreload: false,
+    sourcemap: true,
+    rollupOptions: {
+      treeshake: 'smallest',
+    },
+  },
   resolve: {
     alias: {
       pui: resolve(__dirname, '..'),
