4ef38576eaf3a0304539f16d2a35415a37caafe0
Jason Miller <jason@developit.ca>
2026-02-18 22:47:19 -0500

    support `<Tooltip side="left|right|bottom">`

--- src/components/tooltip/types.ts
+++ src/components/tooltip/types.ts
@@ -6,6 +6,11 @@ export interface TooltipOwnProps extends BaseProps {
    * Tooltip text provided via the title attribute.
    */
   title?: string;
+
+  /**
+   * The direction the tooltip should open towards.
+   */
+  side?: 'left' | 'right' | 'top' | 'bottom' | null;
 }
 
 export type TooltipProps = TooltipOwnProps &
